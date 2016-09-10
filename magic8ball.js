4de6cf22fab26f3086d8090dd1567939afe9757e
jess-alexander <alexanderjessmarie@gmail.com>
2016-09-10 17:05:19 +0200

    Rough Draft of Magic 8 Ball App

---
+++ magic8ball.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+ 
+  console.log("document loaded"); //process check
+
+  $("#magic-eight-ball").submit(function(event) {
+    console.log("inside click function"); //process check
+    event.preventDefault();
+    $('.bottom').show();
+    $('.top').hide();
+    var answers = [
+      'It is certain',
+      'It is decidedly so',
+      'Without a doubt',
+      'Yes, definitely',
+      'You may rely on it',
+      'As I see it, yes',
+      'Most likely',
+      'Outlook good',
+      'Yes',
+      'Signs point to yes',
+      'Reply hazy try again',
+      'Ask again later',
+      'Better not tell you now',
+      'Cannot predict now',
+      'Concentrate and ask again',
+      'Don\'t count on it',
+      'My reply is no',
+      'My sources say no',
+      'Outlook not so good',
+      'Very doubtful'
+    ];
+
+    //math.floor rounds down to make a whole number
+    var randomResponse = answers[Math.floor(Math.random() * 20)]; //random number between 0-19
+
+    console.log(randomResponse); //process check
+
+    $("#answer").text(randomResponse);
+    //display random answer to document
+
+  }); //close form click
+
+  $("#reset-btn").click(function() { //reset the form
+    $('.bottom').hide();
+    $('.top').show();
+  });
+
+});
